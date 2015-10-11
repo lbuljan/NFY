f929b76eac75deac53b5f7cbd883ab2eb18853f1
Dajana Stojanović <daja15@live.com>
2015-10-11 02:25:41 +0200

    proizvodi+opg naslovne responzive

--- opg_naslovna.php
+++ opg_naslovna.php
@@ -19,22 +19,23 @@ $opgi = $opg->fetchAll(PDO::FETCH_OBJ);
 
 
 <div class="row" >
-	<div class="col-xs-12">
-		<h1> Obiteljska poljoprivredna gospodarstva </h1>
-		<hr style="border-color:black;" />
+	
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<h1 class="naslov"> Obiteljska poljoprivredna gospodarstva </h1>
+		<hr style="border-color:black;width:80%;" />
 	</div>
 	<?php foreach($opgi as $op):?>
 	
-		<div class="col-lg-4 col-md-4 col-md-push-1 col-sm-4 col-xs-10 col-xs-push-4">
-		<a href="<?php echo $put;?>user/profil/opg.php?o=<?php echo $op->sifra;?>">
+		<div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-10 col-xs-offset-2 pro">
+		<a href="<?php echo $put;?>user/profil/opg.php?o=<?php echo $op->sifra;?>" class="proizvodi">
 		  
 			<?php if($op->profilna):?>
 				<img class="proizvodi_slika" src="<?php echo $put ?>slike/opg/<?php echo $op->profilna;?>" />
 			<?php else:?>
 				<img class="proizvodi_slika" src="<?php echo $put ?>slike/opg/placeholder.png" />
 			<?php endif;?>
-		</a>
-		<h2><span><?php echo $op->naziv;?></span></h2>
+		
+		<h2><span><?php echo $op->naziv;?></span></h2></a>
 	  </div>
   <?php endforeach;?>
 </div>
